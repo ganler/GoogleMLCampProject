76bd229dcfa56552175f71f7f7309fdc192e48bd
tyuanhang <astridd@sjtu.edu.cn>
2020-01-14 12:33:38 +0800

    create a naive model

--- aisegment/NaiveMatting.py
+++ aisegment/NaiveMatting.py
@@ -66,8 +66,8 @@ df_train = df_train.reset_index(drop=True)
 df_val = df_val.reset_index(drop=True)
 
 # For Debug Use
-# print(df_train)
-# print(df_val)
+# print(df_train)   # 14560 rows
+# print(df_val)     # 3641 rows
 
 df_train.to_csv('df_train.csv.gz', compression='gzip', index=False)
 df_val.to_csv('df_val.csv.gz', compression='gzip', index=False)
@@ -129,3 +129,92 @@ def val_generator(batch_size=100):
 train_gen = train_generator()
 val_gen = val_generator()
 
+from keras.models import Model, load_model
+from keras.layers import Input, UpSampling2D
+from keras.layers.core import Dropout, Lambda
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras import backend as K
+
+import tensorflow as tf
+
+inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+
+s = Lambda(lambda x: x / 255) (inputs)
+
+c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (s)
+c1 = Dropout(0.1) (c1)
+c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c1)
+p1 = MaxPooling2D((2, 2)) (c1)
+
+c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p1)
+c2 = Dropout(0.1) (c2)
+c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c2)
+p2 = MaxPooling2D((2, 2)) (c2)
+
+c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p2)
+c3 = Dropout(0.2) (c3)
+c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c3)
+p3 = MaxPooling2D((2, 2)) (c3)
+
+c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p3)
+c4 = Dropout(0.2) (c4)
+c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c4)
+p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+
+c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p4)
+c5 = Dropout(0.3) (c5)
+c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c5)
+
+u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same') (c5)
+u6 = concatenate([u6, c4])
+c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u6)
+c6 = Dropout(0.2) (c6)
+c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c6)
+
+u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c6)
+u7 = concatenate([u7, c3])
+c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u7)
+c7 = Dropout(0.2) (c7)
+c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c7)
+
+u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c7)
+u8 = concatenate([u8, c2])
+c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u8)
+c8 = Dropout(0.1) (c8)
+c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c8)
+
+u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c8)
+u9 = concatenate([u9, c1], axis=3)
+c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u9)
+c9 = Dropout(0.1) (c9)
+c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c9)
+
+outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
+
+model = Model(inputs=[inputs], outputs=[outputs])
+
+model.compile(optimizer='adam', loss='binary_crossentropy')
+
+num_train_samples = len(df_train)
+num_val_samples = len(df_val)
+train_batch_size = BATCH_SIZE
+val_batch_size = BATCH_SIZE
+
+train_steps = np.ceil(num_train_samples / train_batch_size)
+val_steps = np.ceil(num_val_samples / val_batch_size)
+
+filepath = "model.h5"
+
+earlystopper = EarlyStopping(patience=3, verbose=1)
+
+checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, 
+                             save_best_only=True, mode='min')
+
+callbacks_list = [earlystopper, checkpoint]
+
+history = model.fit_generator(train_gen, steps_per_epoch=train_steps, epochs=20, 
+                                validation_data=val_gen, validation_steps=val_steps, 
+                                verbose=1, callbacks=callbacks_list)
