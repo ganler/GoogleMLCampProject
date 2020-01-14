110ca9c5a6a147478c59f9ea081ee78a7c3e24e5
tyuanhang <astridd@sjtu.edu.cn>
2020-01-14 12:05:00 +0800

    modify the file path

--- aisegment/NaiveMatting.py
+++ aisegment/NaiveMatting.py
@@ -15,19 +15,19 @@ IMG_CHANNELS = 3
 BATCH_SIZE = 100
 
 root_path = '../../ml-camp/MattingHuman/aisegmentcom-matting-human-datasets'
-clip_root_path = os.path.join(root_path + 'clip_img')
-matting_root_path = os.path.join(root_path + 'matting')
+clip_root_path = os.path.join(root_path, 'clip_img')
+matting_root_path = os.path.join(root_path, 'matting')
 
 df_clip = pd.DataFrame()
 # eg. first_folder = 1803151818
 # eg. second_folder = clip_00000000
 first_folder_list = os.listdir(clip_root_path)
 for i, first_folder in enumerate(first_folder_list):
-    second_folder_list = os.listdir(os.path.join(clip_root_path + first_folder))
+    second_folder_list = os.listdir(os.path.join(clip_root_path, first_folder))
     for j, second_folder in enumerate(second_folder_list):
-        image_list = os.listdir(os.path.join(clip_root_path + first_folder + second_folder))
+        image_list = os.listdir(os.path.join(clip_root_path, first_folder, second_folder))
         df_tmp = pd.DataFrame(image_list, columns=['clip_id'])
-        df_tmp['clip_path'] = os.path.join(clip_root_path + first_folder + second_folder)
+        df_tmp['clip_path'] = os.path.join(clip_root_path, first_folder, second_folder)
         df_clip = pd.concat([df_clip, df_tmp], axis=0).reset_index(drop=True)
 print(df_clip.head())
 print(df_clip.tail())
